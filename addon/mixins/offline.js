863d64ef77eb62e3141ef6c969f44c234db16b43
Opak Alex <opak.alexandr@gmail.com>
2015-06-06 15:17:55 +0300

    fix tests

--- addon/mixins/offline.js
+++ addon/mixins/offline.js
@@ -66,33 +66,31 @@ export default Mixin.create({
     return adapterResp;
   },
 
-  find: function(store, type, id, snapshot) {
-    if (this.get('isOnline')) {
-      let onlineResp = this._super.apply(this, arguments);
-      let adapter = this;
-      //check offline storage
-      Ember.RSVP.resolve().then(() => {
-        return this.get('offlineAdapter').find(store, type, id, snapshot);
-      }).then(offineRecord => {
-        if (Ember.isEmpty(offineRecord)) {
-          return adapterResp;
-        }
-      }).then(onlineRecord => {
-        if (!Ember.isEmpty(onlineRecord)) {
-          this.get('offlineAdapter').persistData(typeClass, onlineRecord);
-        }
-      }).catch(() => {
-          onlineResp.then(onlineRecord => {
-            if (!Ember.isEmpty(onlineRecord)) {
-              adapter.get('offlineAdapter').persistData(typeClass, onlineRecord);
-            }
-          });
-      });
-
-      return onlineResp;
-    } else {
-      return this.get('offlineAdapter').find(store, type, id, snapshot);
+  find: function(store, typeClass, id, snapshot) {
+    if (this.get('isOffline')) {
+      return this.get('offlineAdapter').find(store, typeClass, id, snapshot);
     }
+    let onlineResp = this._super.apply(this, arguments);
+    let adapter = this;
+    //check offline storage
+    Ember.RSVP.resolve().then(() => {
+      return this.get('offlineAdapter').find(store, typeClass, id, snapshot);
+    }).then(offineRecord => {
+      if (Ember.isEmpty(offineRecord)) {
+        return onlineResp;
+      }
+    }).then(onlineRecord => {
+      if (!Ember.isEmpty(onlineRecord)) {
+        this.get('offlineAdapter').persistData(typeClass, onlineRecord);
+      }
+    }).catch(() => {
+        onlineResp.then(onlineRecord => {
+          if (!Ember.isEmpty(onlineRecord)) {
+            adapter.get('offlineAdapter').persistData(typeClass, onlineRecord);
+          }
+        });
+    });
+    return onlineResp;
   },
 
   assertRunner: on('init', function() {

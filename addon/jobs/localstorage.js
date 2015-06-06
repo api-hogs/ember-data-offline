9da65579ad033f15d844eb49e17f226048067b6d
iggor_kurr <kurinnoyi@gmail.com>
2015-06-06 22:06:14 +0300

    pretify import

--- addon/jobs/localstorage.js
+++ addon/jobs/localstorage.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 import jobMixin from 'ember-data-offline/mixins/job';
 
+const { isEmpty, RSVP } = Ember;
+
 export default Ember.Object.extend(jobMixin, {
   task() {
     return this[this.get('method')].apply(this.get('adapter'), this.get('params'));
@@ -9,7 +11,7 @@ export default Ember.Object.extend(jobMixin, {
   findAll(store, typeClass, sinceToken, adapterResp){
     let offlineAdapter = this.get('offlineAdapter');
     adapterResp.then(records => {
-      if (!Ember.isEmpty(records)) {
+      if (!isEmpty(records)) {
         offlineAdapter.persistData(typeClass, records);
       }
     });
@@ -18,24 +20,23 @@ export default Ember.Object.extend(jobMixin, {
   find(store, typeClass, id, snapshot, onlineResp){
     let adapter = this;
     let offlineAdapter = adapter.get('offlineAdapter');
-    //check offline storage
-    Ember.RSVP.resolve().then(() => {
+
+    RSVP.resolve().then(() => {
       return offlineAdapter.find(store, typeClass, id, snapshot);
     }).then(offineRecord => {
-      if (Ember.isEmpty(offineRecord)) {
+      if (isEmpty(offineRecord)) {
         return onlineResp;
       }
     }).then(onlineRecord => {
-      if (!Ember.isEmpty(onlineRecord)) {
+      if (!isEmpty(onlineRecord)) {
         offlineAdapter.persistData(typeClass, onlineRecord);
       }
     }).catch(() => {
         onlineResp.then(onlineRecord => {
-          if (!Ember.isEmpty(onlineRecord)) {
+          if (!isEmpty(onlineRecord)) {
             offlineAdapter.persistData(typeClass, onlineRecord);
           }
         });
     });
   }
-
 });

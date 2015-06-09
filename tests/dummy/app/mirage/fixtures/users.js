83c97ebe10fc2e1aefc8c809f0bce2db91f6b6ae
Opak Alex <opak.alexandr@gmail.com>
2015-06-09 19:52:17 +0300

    fix for real project

--- tests/dummy/app/mirage/fixtures/users.js
+++ tests/dummy/app/mirage/fixtures/users.js
@@ -1,14 +1,14 @@
-export default [
-  {
-    id: 1,
-    firstName: 'Zelda',
-    lastName: 'Ogge',
-    gender: 'male'
-  },
-  {
-    id: 2,
-    firstName: 'Zelda-2',
-    lastName: 'Ogge-2',
-    gender: 'male'
-  },
-];
+// export default [
+//   {
+//     id: 1,
+//     firstName: 'Zelda',
+//     lastName: 'Ogge',
+//     gender: 'male'
+//   },
+//   {
+//     id: 2,
+//     firstName: 'Zelda-2',
+//     lastName: 'Ogge-2',
+//     gender: 'male'
+//   },
+// ];

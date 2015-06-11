af81840530aca83dc7f8ae1bed60b2b5a7b03826
iggor_kurr <kurinnoyi@gmail.com>
2015-06-11 18:27:02 +0300

    refactor

--- addon/jobs/localstorage.js
+++ addon/jobs/localstorage.js
@@ -11,28 +11,46 @@ export default Ember.Object.extend(jobMixin, {
   findAll(store, typeClass, sinceToken, adapterResp) {
     let offlineAdapter = this.get('adapter');
     adapterResp.then(records => {
-      let fromStore = store.all(typeClass);
-      if (!Ember.isEmpty(fromStore)) {
-        fromStore.forEach(record => {
-          let snapshot = record._createSnapshot();
-          offlineAdapter.createRecord(store, typeClass, snapshot);
-        });
-      }
+      this.persistOffline(store, typeClass, records, 'findAll');
     });
+  },
+
+  persistOffline(store, typeClass, onlineResp, method) {
+    if (Ember.isEmpty(onlineResp)) {
+     return; 
+    }
+    if (method === 'find') {
+     this._persistOne(store, typeClass, onlineResp);
+    }
+    else {
+     this._persistMany(store, typeClass);
+    }
   },
 
   _persistOne(store, typeClass, onlineRecord){
     let fromStore = store.all(typeClass);
-    if (!Ember.isEmpty(fromStore)) {
-      let recordFromStore = fromStore.find(record => {
-        return record.id === onlineRecord[typeClass.modelName].id;
-      });
-      let snapshot = recordFromStore._createSnapshot();
-      this.get('adapter').createRecord(store, typeClass, snapshot);
+    if (Ember.isEmpty(fromStore)) {
+      return;
     }
+    let recordFromStore = fromStore.find(record => {
+      return record.id === onlineRecord[typeClass.modelName].id;
+    });
+    let snapshot = recordFromStore._createSnapshot();
+    this.get('adapter').createRecord(store, typeClass, snapshot);
   },
 
-  _findWithCheck: function(onlineResp, store, typeClass, ...params) {
+  _persistMany(store, typeClass){
+    let fromStore = store.all(typeClass);
+    if (Ember.isEmpty(fromStore)) {
+      return;
+    }
+    fromStore.forEach(record => {
+      let snapshot = record._createSnapshot();
+      this.get('adapter').createRecord(store, typeClass, snapshot);
+    });
+  },
+
+  _findWithCheck: function(method, onlineResp, store, typeClass, ...params) {
     let offlineAdapter = this.get('adapter');
 
     RSVP.resolve().then(() => {
@@ -42,24 +60,24 @@ export default Ember.Object.extend(jobMixin, {
         return onlineResp;
       }
     }).then(onlineRecord => {
-      this._persistOne(store, typeClass, onlineRecord);
+      this.persistOffline(store, typeClass, onlineRecord, method);
     }).catch(() => {
       onlineResp.then(onlineRecord => {
-        this._persistOne(store, typeClass, onlineRecord);
+        this.persistOffline(store, typeClass, onlineRecord, method);
       });
     });
   },
 
   find(store, typeClass, id, snapshot, onlineResp) {
-    this._findWithCheck(onlineResp, store, typeClass, id, snapshot);
+    this._findWithCheck('find', onlineResp, store, typeClass, id, snapshot);
   },
 
   findQuery(store, typeClass, query, onlineResp) {
-    this._findWithCheck(onlineResp, store, typeClass, query);
+    this._findWithCheck('findQuery', onlineResp, store, typeClass, query);
   },
 
   findMany(store, typeClass, ids, snapshots, onlineResp) {
-    this._findWithCheck(onlineResp, store, typeClass, ids, snapshots);
+    this._findWithCheck('findMany', onlineResp, store, typeClass, ids, snapshots);
   },
 
   createRecord(store, type, snapshot, onlineResp){

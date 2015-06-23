c50674cd0b69a15037f99a8a5d5b4e0426cf1fbe
iggor_kurr <kurinnoyi@gmail.com>
2015-06-23 15:36:10 +0300

    try fix

--- addon/mixins/online.js
+++ addon/mixins/online.js
@@ -21,7 +21,7 @@ export default Ember.Mixin.create(baseMixin, {
         this.createOfflineJob('find', [store, typeClass, id, snapshot, onlineResp, true], store);
       }
       return resp;
-    });
+    }).catch(console.log.bind(console));
   },
 
   findQuery: function(store, type, query, fromJob) {

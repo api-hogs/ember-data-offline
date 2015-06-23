0ad6f8a1d3385cee1e172ca4400c34addbc5e912
iggor_kurr <kurinnoyi@gmail.com>
2015-06-23 15:22:54 +0300

    try fix errors

--- addon/mixins/online.js
+++ addon/mixins/online.js
@@ -5,35 +5,43 @@ export default Ember.Mixin.create(baseMixin, {
 
   findAll: function(store, typeClass, sinceToken, fromJob) {
     let adapterResp = this._super.apply(this, arguments);
-    if (!fromJob) {
-      this.createOfflineJob('findAll', [store, typeClass, sinceToken, adapterResp, true], store);
-    }
-    console.log('findAll from online adapter', typeClass);
-    return adapterResp;
+    return adapterResp.then(resp => {
+      if (!fromJob) {
+        this.createOfflineJob('findAll', [store, typeClass, sinceToken, adapterResp, true], store);
+      }
+      console.log('findAll from online adapter', typeClass);
+      return resp;
+    });
   },
 
   find: function(store, typeClass, id, snapshot, fromJob) {
     let onlineResp = this._super.apply(this, arguments);
-    if (!fromJob) {
-      this.createOfflineJob('find', [store, typeClass, id, snapshot, onlineResp, true], store);
-    }
-    return onlineResp;
+    return onlineResp.then(resp => {
+      if (!fromJob) {
+        this.createOfflineJob('find', [store, typeClass, id, snapshot, onlineResp, true], store);
+      }
+      return resp;
+    });
   },
 
   findQuery: function(store, type, query, fromJob) {
     let onlineResp = this._super.apply(this, arguments);
-    if (!fromJob) {
-      this.createOfflineJob('findQuery', [store, type, query, onlineResp, true], store);
-    }
-    return onlineResp;
+    return onlineResp.then(resp => {
+      if (!fromJob) {
+        this.createOfflineJob('findQuery', [store, type, query, onlineResp, true], store);
+      }
+      return resp;
+    });
   },
 
   findMany: function(store, type, ids, snapshots, fromJob) {
     let onlineResp = this._super.apply(this, arguments);
-    if (!fromJob) {
-      this.createOfflineJob('find', [store, type, ids, snapshots, onlineResp, true], store);
-    }
-    return onlineResp;
+    return onlineResp.then(resp => {
+      if (!fromJob) {
+        this.createOfflineJob('find', [store, type, ids, snapshots, onlineResp, true], store);
+      }
+      return resp;
+    });
   },
 
   createRecord(store, type, snapshot, fromJob) {

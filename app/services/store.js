f0f16640241c50ca4014409fc4f0742e520efa81
opak <opak.alexandr@gmail.com>
2015-07-14 19:38:41 +0300

    Update store.js

--- app/services/store.js
+++ app/services/store.js
@@ -3,9 +3,5 @@ import DS from 'ember-data';
 import syncLoads from 'ember-data-offline/utils/sync-loads';
 
 export default DS.Store.extend({
-  syncLoads: syncLoads.create(),
 
-  adapterFor: function() {
-    return this._super.apply(this, arguments).get('offlineAdapter');
-  },
 });

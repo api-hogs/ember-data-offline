d52a49f28189c2e0e75bd8ac405a305c4ea3bfc0
opak <opak.alexandr@gmail.com>
2015-07-14 18:55:38 +0300

    Update debug.js

--- addon/utils/debug.js
+++ addon/utils/debug.js
@@ -3,8 +3,5 @@ const { debug } = Ember;
 
 //Later we can remove this, but until stable release we need it
 export default function() {
-  let message = Array.from(arguments).reduce((p, n) => {
-    return p + n + " :: ";
-  }, "");
-  debug(`[ember-data-offline]: ${message}`);
+  debug(`[ember-data-offline]: xyi`);
 }

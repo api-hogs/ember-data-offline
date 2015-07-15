48b07d33423b61208a9fdedebaaba9ac87ee2b4f
iggor_kurr <kurinnoyi@gmail.com>
2015-07-15 18:30:14 +0300

    remove these fucking queue initializers

--- app/services/store.js
+++ app/services/store.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 import syncLoads from 'ember-data-offline/logics/sync-loads';
+import Queue from 'ember-data-offline/queue';
 
 export default DS.Store.extend({
   syncLoads: syncLoads.create(),
+  EDOQueue: Queue.create(),
 
   adapterFor: function() {
     return this._super.apply(this, arguments).get('offlineAdapter');

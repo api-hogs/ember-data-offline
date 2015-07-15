3af5eae6d59669e4455a4d6b6e003ded2487f235
iggor_kurr <kurinnoyi@gmail.com>
2015-07-15 12:57:03 +0300

    back fix

--- addon/mixins/online.js
+++ addon/mixins/online.js
@@ -50,19 +50,19 @@ export default Ember.Mixin.create(baseMixin, {
   findMany: function(store, type, ids, snapshots, fromJob) {
     debug('findMany online', type.modelName, ids);
     let onlineResp;
-    let recordsInStore = store.peekAll(type.modelName);
-    let inStoreIds = recordsInStore.map(record => {
-      return !isObjectEmpty(record._internalModel._data) && record.id;
-    });
+    // let recordsInStore = store.peekAll(type.modelName);
+    // let inStoreIds = recordsInStore.map(record => {
+    //   return !isObjectEmpty(record._internalModel._data) && record.id;
+    // });
 
-    let idsDiff = ids.filter(item => inStoreIds.indexOf(item) < 0);
+    // let idsDiff = ids.filter(item => inStoreIds.indexOf(item) < 0);
 
-    if (!Ember.isEmpty(idsDiff)) {
+    // if (!Ember.isEmpty(idsDiff)) {
       onlineResp = this._super(store, type, idsDiff, snapshots);
-    }
-    else {
-      onlineResp = Ember.RSVP.resolve([]);
-    }
+    // }
+    // else {
+    //   onlineResp = Ember.RSVP.resolve([]);
+    // }
 
     return onlineResp.then(resp => {
       if (!fromJob) {

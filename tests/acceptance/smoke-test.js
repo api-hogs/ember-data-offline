070b47a1d33dbc1764ae800e4ccb1bbe29e9f489
Anatolii Domaratskyi <domaratskyias@outlook.com>
2015-07-23 12:27:07 +0300

    fix acceptence #1

--- tests/acceptance/smoke-test.js
+++ tests/acceptance/smoke-test.js
@@ -38,10 +38,10 @@ test('localforage is populated on #findAll', function(assert) {
       return lfItems.then(result => {
         assert.equal(result.user.records[1].firstName, users[0].firstName, "Record 1 from server === record 1 in locaclforage");
         assert.equal(result.user.records[2].firstName, users[1].firstName, "Record 2 from server === record 2 in locaclforage");
-        assert.equal(store.peelAll('user').get('firstObject').firstName, users[0].firstName, "Record 1 in store === record 1 from server ");
-        assert.equal(store.peelAll('user').get('lastObject').firstName, users[1].firstName, "Record 2 in store === record 2 from server ");
-        assert.equal(store.peelAll('user').get('firstObject').firstName, result.user.records[1].firstName, "Record 1 in store === record 1 in localforage");
-        assert.equal(store.peelAll('user').get('lastObject').firstName, result.user.records[2].firstName, "Record 2 in store === record 2 in localforage");
+        assert.equal(store.peekAll('user').get('firstObject').get('firstName'), users[0].firstName, "Record 1 in store === record 1 from server ");
+        assert.equal(store.peekAll('user').get('lastObject').get('firstName'), users[1].firstName, "Record 2 in store === record 2 from server ");
+        assert.equal(store.peekAll('user').get('firstObject').get('firstName'), result.user.records[1].firstName, "Record 1 in store === record 1 in localforage");
+        assert.equal(store.peekAll('user').get('lastObject').get('firstName'), result.user.records[2].firstName, "Record 2 in store === record 2 in localforage");
         start();
       });
     },3000);

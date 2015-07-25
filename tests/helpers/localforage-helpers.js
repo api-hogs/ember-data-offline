9cf0f953061c4098d8195973eb093be4fb09bd21
iggor_kurr <kurinnoyi@gmail.com>
2015-07-25 13:49:06 +0300

    remove unused test helpers

--- tests/helpers/localforage-helpers.js
+++ tests/helpers/localforage-helpers.js
@@ -2,28 +2,6 @@ import Ember from "ember";
 
 var localforageHelpers = function() {
 
-  Ember.Test.registerAsyncHelper('waitForRecordingAll', function(app, modelsToProccess) {
-    return Ember.Test.promise(function(resolve) {
-      Ember.Test.adapter.asyncStart();
-      let interval = window.setInterval(function() {
-
-        let recordingEnded = Object.keys(modelsToProccess).map(model => {
-          let adapter = app.__container__.lookup('service:store').adapterFor(model);
-          let queue = adapter.get('queue.queue');
-          return queue.length === modelsToProccess[model] + 1;
-        }).reduce((p, n) => {
-          return p && n;
-        }, true);
-
-        if (recordingEnded) {
-          window.clearInterval(interval);
-          Ember.Test.adapter.asyncEnd();
-          Ember.run(null, resolve, true);
-        }
-      }, 100);
-    });
-  });
-
   Ember.Test.registerAsyncHelper('waitForRecordingModel', function(app, model, numberOfRecords = 1) {
     return Ember.Test.promise(function(resolve) {
       Ember.Test.adapter.asyncStart();
@@ -39,23 +17,6 @@ var localforageHelpers = function() {
           });
         }
       }, 100);
-    });
-  });
-
-  Ember.Test.registerAsyncHelper('getLocalforageData', function(app, hash, modelToCheck) {
-    return Ember.Test.promise(function(resolve) {
-      waitForRecordingAll(hash).then(() => {
-
-        let adapter = app.__container__.lookup('service:store').adapterFor(modelToCheck);
-        let queue = adapter.get('queue.queue');
-        queue[queue.length - 1]
-          .then(() => {
-            return window.localforage.getItem("foo");
-          })
-          .then(records => {
-            resolve(records);
-          });
-      });
     });
   });
 

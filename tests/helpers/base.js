f4f28af1ae264719976984cde8942ae9c22e82d6
Anatolii Domaratskyi <domaratskyias@outlook.com>
2015-07-30 18:45:16 +0300

    some fixes for tests

--- tests/helpers/base.js
+++ tests/helpers/base.js
@@ -30,10 +30,15 @@ var goOnline = function goOnline() {
 
 var getStoreMock = function() {
   return Ember.Object.create({
-    lookupAdapter() {
-      return Ember.Object.create({
-        recordTTL : moment.duration(12, 'hours')
-      });
+    peekAll(){
+      return Ember.A([
+        getSnapshotMock()
+      ]);
+    },
+    peekRecord(modelName, id){
+      if(id === 'foo'){
+        return getSnapshotMock();
+      }
     },
     serializerFor(){
       return {
@@ -45,7 +50,44 @@ var getStoreMock = function() {
           return key;
         }
       };
+    },
+    metadataFor(){
+      return Ember.Object.create();
+    },
+    lookupAdapter() {
+      return Ember.Object.create({
+        recordTTL : moment.duration(12, 'hours')
+      });
     }
+  });
+};
+
+var getResultMock = function() {
+  return {
+    name: 'foo'
+  };
+};
+
+var getResultFromPayloadMock = function() {
+  return {
+    name: 'foo2'
+  };
+};
+
+var getTypeMock = function(){
+    return Ember.Object.create({
+      modelName : 'bar'
+    });
+};
+
+var getSnapshotMock = function() {
+  return Ember.Object.create({
+    record : Ember.Object.create({
+      store : getStoreMock(),
+      __data_offline_meta__ : Ember.Object.create()
+    }),
+    id : 'foo',
+    _internalModel : getTypeMock()
   });
 };
 
@@ -54,9 +96,64 @@ var getQueueMock = function(assert, encapsulatedIn) {
     _assert: assert,
     _encapsulatedIn: encapsulatedIn,
     add() {
-      this.get('_assert').ok(true, 'queue.add was invoked @' + this.get('_encapsulatedIn'));
+      this.get('_assert').ok(true, `queue.add was invoked @ ${this.get('_encapsulatedIn')}`);
     }
   });
 };
 
-export {goOnline, goOffline, getStoreMock, getQueueMock};
+var getMetaMock = function(){
+  return {
+    __data_offline_meta__ : {
+      fetchedAt : moment()
+    }
+  };
+};
+
+//setup assert
+var getAdapterMock = function(adapterName){
+  return Ember.Object.create({
+    __adapterName__: adapterName,
+    findAll(){
+      this.get('assert').ok(true, `findAll was invoked @ ${this.get('__adapterName__')} adapter`);
+      return Ember.RSVP.Promise.resolve(Ember.A([getResultMock()]));
+    },
+    find(){
+      this.get('assert').ok(true, `find was invoked @ ${this.get('__adapterName__')} adapter`);
+      //simulate if param id equals 2 then return empty record
+      return (arguments[2] === 'no_record') ? Ember.RSVP.Promise.resolve() : Ember.RSVP.Promise.resolve(getResultMock());
+    },
+    query(){
+      this.get('assert').ok(true, `query was invoked @ ${this.get('__adapterName__')} adapter`);
+      //simulate if param id equals 2 then return empty record
+      return (arguments[2] === 'no_record') ? Ember.RSVP.Promise.resolve(Ember.A()) : Ember.RSVP.Promise.resolve(Ember.A([getResultMock()]));
+    },
+    findMany(){
+      this.get('assert').ok(true, `findMany was invoked @ ${this.get('__adapterName__')} adapter`);
+      //simulate if param id equals 2 then return empty record
+      return (arguments[2][0] === 'no_record') ? Ember.RSVP.Promise.resolve(Ember.A()) : Ember.RSVP.Promise.resolve(Ember.A([getResultMock()]));
+    },
+    createRecord(){
+      this.get('assert').ok(true, `createRecord was invoked @ ${this.get('__adapterName__')} adapter`);
+      return Ember.RSVP.Promise.resolve(getSnapshotMock());
+    },
+    updateRecord(){
+      this.get('assert').ok(true, `updateRecord was invoked @ ${this.get('__adapterName__')} adapter`);
+      return Ember.RSVP.Promise.resolve(getSnapshotMock());
+    },
+    deleteRecord(){
+      this.get('assert').ok(true, `deleteRecord was invoked @ ${this.get('__adapterName__')} adapter`);
+      return Ember.RSVP.Promise.resolve(getSnapshotMock());
+    },
+    _namespaceForType(){
+      this.get('assert').ok(true, `_namespaceForType was invoked @ ${this.get('__adapterName__')} adapter`);
+      let metaMock = getMetaMock();
+      metaMock.fetchedAt = moment().subtract(13, 'hours').calendar(); //outdated because ttl = 12 hours
+      return Ember.RSVP.Promise.resolve(metaMock);
+    }
+  });
+};
+
+export {
+  goOnline, goOffline, getAdapterMock, getStoreMock, getQueueMock, getSnapshotMock, getMetaMock,
+  getTypeMock, getResultMock, getResultFromPayloadMock
+};
--- tests/unit/mixins/offline-test.js
+++ tests/unit/mixins/offline-test.js
@@ -5,77 +5,35 @@ import Ember from 'ember';
 import OfflineMixin from 'ember-data-offline/mixins/offline';
 import Queue from 'ember-data-offline/queue';
 import { module, test } from 'qunit';
-import { getStoreMock, getQueueMock } from '../../helpers/base';
-import { storeMock, snapshotMock} from '../../helpers/job';
+import {
+  getStoreMock, getQueueMock, getSnapshotMock, getAdapterMock,
+  getTypeMock, getResultMock, getResultFromPayloadMock
+} from '../../helpers/base';
 import moment from 'moment';
 
 
-var subject;
-
-var singleResultMock = {
-  name : 'foo'
-};
-
-var resultFromPayload = {
-  name : 'foo2'
-};
-
-var typeClassMock = Ember.Object.create({
-  modelName : 'bar'
-});
+var subject, store, typeClass, snapshot, expectedResult, expectedResultFromPayload;
 
 module('Unit | Mixin | offline',  {
     beforeEach: function(){
-      subject = Ember.Object.extend( {
-        __adapterName__: "OFFLINE",
-        findAll(){
-          this.get('assert').ok(true, 'findAll was invoked @ offline adapter');
-          return Ember.RSVP.Promise.resolve(Ember.A([singleResultMock]));
-        },
-        find(){
-          this.get('assert').ok(true, 'find was invoked @ offline adapter');
-          //simulate if param id equals 2 then return empty record
-          return (arguments[2] === 'no_record') ? Ember.RSVP.Promise.resolve() : Ember.RSVP.Promise.resolve(singleResultMock);
-        },
-        query(){
-          this.get('assert').ok(true, 'query was invoked @ offline adapter');
-          //simulate if param id equals 2 then return empty record
-          return (arguments[2] === 'no_record') ? Ember.RSVP.Promise.resolve(Ember.A()) : Ember.RSVP.Promise.resolve(Ember.A([singleResultMock]));
-        },
-        findMany(){
-          this.get('assert').ok(true, 'findMany was invoked @ offline adapter');
-          //simulate if param id equals 2 then return empty record
-          return (arguments[2][0] === 'no_record') ? Ember.RSVP.Promise.resolve(Ember.A()) : Ember.RSVP.Promise.resolve(Ember.A([singleResultMock]));
-        },
-        createRecord(){
-          this.get('assert').ok(true, 'createRecord was invoked @ offline adapter');
-          return Ember.RSVP.Promise.resolve(snapshotMock);
-        },
-        updateRecord(){
-          this.get('assert').ok(true, 'udapteRecord was invoked @ offline adapter');
-          return Ember.RSVP.Promise.resolve(snapshotMock);
-        },
-        deleteRecord(){
-          this.get('assert').ok(true, 'deleteRecord was invoked @ offline adapter');
-          return Ember.RSVP.Promise.resolve(snapshotMock);
-        },
-        _namespaceForType(){
-          this.get('assert').ok(true, '_namespaceForType was invoked @ offline adapter');
-          return Ember.RSVP.Promise.resolve({
-            __data_offline_meta__ : {
-              fetchedAt: moment().subtract(13, 'hours').calendar() //outdated because ttl = 12 hours
-            }
-          });
-        },
+
+      store = getStoreMock();
+      snapshot = getSnapshotMock();
+      typeClass = getTypeMock();
+      expectedResult = getResultMock();
+      expectedResultFromPayload = getResultFromPayloadMock();
+
+      let offlineAdapterMock = getAdapterMock("OFFLINE");
+
+      subject = Ember.Object.extend(offlineAdapterMock,{
         onlineAdapter : Ember.Object.create({
           //assert : this.get('assert'),
           findQuery(){
             this.get('assert').ok(true, 'query was invoked @ online adapter');
-            return Ember.RSVP.Promise.resolve({ bar: Ember.A([resultFromPayload])});
+            return Ember.RSVP.Promise.resolve({ bar: Ember.A([expectedResultFromPayload])});
           }
         })
       }).extend(OfflineMixin).create();
-
     },
     afterEach: function(){
       subject = null;
@@ -86,36 +44,33 @@ module('Unit | Mixin | offline',  {
 test('findAll',(assert) => {
   assert.expect(2);
 
-  let store = storeMock.create();
   subject.set('assert', assert);
 
   //2 asserts : adapter.findAll + equal
   stop();
-  subject.findAll(store, typeClassMock, 'sinceToken', { id: 'foo'}, true).then((result)=>{
-    assert.equal(result[0], singleResultMock, "returns result");
+  subject.findAll(store, typeClass, 'sinceToken', [snapshot], true).then((result)=>{
+    assert.equal(result[0].name, expectedResult.name);
     start();
   });
-
   //TODO TEST WITH fromJom parameter
 });
 
 test('find', (assert)=>{
   assert.expect(4);
 
-  let store = storeMock.create();
   subject.set('assert', assert);
 
   //2 asserts : adapter.find + equal
   stop();
-  subject.find(store, typeClassMock, 'foo', { id: 'foo'}, true).then((result)=>{
-    assert.equal(result, singleResultMock, "returns result");
+  subject.find(store, typeClass, 'foo', snapshot, true).then((result)=>{
+    assert.equal(result.name, expectedResult.name);
     start();
   });
 
   //2 asserts : adapter.find + equal
   stop();
-  subject.find(store, typeClassMock, 'no_record', { id: 'foo'}, true).then((result)=>{
-    assert.equal(result.id, 'no_record', "returns stub");
+  subject.find(store, typeClass, 'no_record', snapshot, true).then((result)=>{
+    assert.equal(result.id, 'no_record');
     start();
   });
   //TODO TEST WITH fromJom parameter
@@ -124,13 +79,12 @@ test('find', (assert)=>{
 test('query', (assert)=>{
   assert.expect(5);
 
-  let store = storeMock.create();
   subject.set('assert', assert);
 
   //2 asserts : adapter.query + equal
   stop();
-  subject.query(store, typeClassMock, {name : 'foo'}, [{ id: 'foo'}], true).then((result)=>{
-    assert.equal(result[0], singleResultMock, "returns result");
+  subject.query(store, typeClass, {name : 'foo'}, [snapshot], true).then((result)=>{
+    assert.equal(result[0].name, expectedResult.name);
     start();
   });
 
@@ -138,8 +92,8 @@ test('query', (assert)=>{
 
   //3 asserts : adapter.query + onlineAdapter.findQuery + equal
   stop();
-  subject.query(store, typeClassMock, 'no_record', [{ id: 'foo'}], true).then((result)=>{
-    assert.equal(result[0], resultFromPayload, "returns result");
+  subject.query(store, typeClass, 'no_record', [snapshot], true).then((result)=>{
+    assert.equal(result[0].name, expectedResultFromPayload.name);
     start();
   });
   //TODO TEST WITH fromJom parameter=false
@@ -149,19 +103,18 @@ test('query', (assert)=>{
 test('findMany', (assert)=>{
   assert.expect(4);
 
-  let store = storeMock.create();
   subject.set('assert', assert);
 
   //2 asserts : adapter.findMany + equal
   stop();
-  subject.findMany(store, typeClassMock, ['foo'], [{ id: 'foo'}], true).then((result)=>{
-    assert.equal(result[0], singleResultMock, "returns result");
+  subject.findMany(store, typeClass, ['foo'], [snapshot], true).then((result)=>{
+    assert.equal(result[0].name, expectedResult.name);
     start();
   });
 
-  //3 asserts : adapter.findMany  + equal
+  //2 asserts : adapter.findMany  + equal
   stop();
-  subject.findMany(store, typeClassMock, ['no_record'], [{ id: 'foo'}], true).then((result)=>{
+  subject.findMany(store, typeClass, ['no_record'], [snapshot], true).then((result)=>{
     assert.equal(result[0].id, 'no_record', "returns stub");
     start();
   });
@@ -169,76 +122,65 @@ test('findMany', (assert)=>{
 });
 
 
-test('createRecord', (assert)=>{
+test('createRecord', (assert) => {
   assert.expect(5);
-
-  let store = storeMock.create();
 
   subject.set('assert', assert);
 
   //2 asserts : adapter.createRecord + equal
   stop();
-  subject.createRecord(store, typeClassMock, snapshotMock, true).then((result)=>{
-    assert.equal(result,snapshotMock, "returns result");
+  subject.createRecord(store, typeClass, snapshot, true).then((result) => {
+    assert.ok(result);
     start();
   });
 
   //3 asserts : adapter.createRecord + queue.add + equal
-  subject.set('EDOQueue', getQueueMock(assert,'oflineMixin'));
+  subject.set('EDOQueue', getQueueMock(assert, 'oflineMixin'));
   stop();
-  subject.createRecord(store, typeClassMock, snapshotMock, false).then((result)=>{
-    assert.equal(result,snapshotMock, "returns result");
+  subject.createRecord(store, typeClass, snapshot, false).then((result) => {
+    assert.ok(result);
     start();
   });
-
 });
 
-
-test('updateRecord', (assert)=>{
+test('updateRecord', (assert) => {
   assert.expect(5);
-
-  let store = storeMock.create();
 
   subject.set('assert', assert);
 
   //2 asserts : adapter.updateRecord + equal
   stop();
-  subject.updateRecord(store, typeClassMock, snapshotMock, true).then((result)=>{
-    assert.equal(result,snapshotMock, "returns result");
+  subject.updateRecord(store, typeClass, snapshot, true).then((result) => {
+    assert.ok(result);
     start();
   });
 
   //3 asserts : adapter.updateRecord + queue.add + equal
-  subject.set('EDOQueue', getQueueMock(assert,'oflineMixin'));
+  subject.set('EDOQueue', getQueueMock(assert, 'oflineMixin'));
   stop();
-  subject.updateRecord(store, typeClassMock, snapshotMock, false).then((result)=>{
-    assert.equal(result,snapshotMock, "returns result");
+  subject.updateRecord(store, typeClass, snapshot, false).then((result) => {
+    assert.ok(result);
     start();
   });
-
 });
 
-
-test('deleteRecord', (assert)=>{
+test('deleteRecord', (assert) => {
   assert.expect(5);
-
-  let store = storeMock.create();
 
   subject.set('assert', assert);
 
   //2 asserts : adapter.deleteRecord + equal
   stop();
-  subject.deleteRecord(store, typeClassMock, snapshotMock, true).then((result)=>{
-    assert.equal(result,snapshotMock, "returns result");
+  subject.deleteRecord(store, typeClass, snapshot, true).then((result) => {
+    assert.ok(result);
     start();
   });
 
   //3 asserts : adapter.deleteRecord + queue.add + equal
-  subject.set('EDOQueue', getQueueMock(assert,'oflineMixin'));
+  subject.set('EDOQueue', getQueueMock(assert, 'oflineMixin'));
   stop();
-  subject.deleteRecord(store, typeClassMock, snapshotMock, false).then((result)=>{
-    assert.equal(result,snapshotMock, "returns result");
+  subject.deleteRecord(store, typeClass, snapshot, false).then((result) => {
+    assert.ok(result);
     start();
   });
-
 });

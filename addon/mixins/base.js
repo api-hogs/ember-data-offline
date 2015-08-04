6b059556b3d00d97e31e740a0c0709880c9c6465
Anatolii Domaratskyi <domaratskyias@outlook.com>
2015-08-04 19:15:14 +0300

    init docs. added first docs.

--- addon/mixins/base.js
+++ addon/mixins/base.js
@@ -1,3 +1,7 @@
+/**
+@module ember-data-offline
+**/
+
 import Ember from 'ember';
 import onlineJob from 'ember-data-offline/jobs/rest';
 import offlineJob from 'ember-data-offline/jobs/localforage';
@@ -5,15 +9,44 @@ import moment from 'moment';
 
 const { Mixin, $, on,  computed, get, isPresent } = Ember;
 
+/**
+@class Base
+@constructor
+**/
 export default Mixin.create({
+  /**
+  Shows if adapter is offline.
+  @property isOffline
+  @type {boolean}
+  **/
   isOffline: computed.not('isOnline'),
+  /**
+  @private
+  @property onlineJob
+  @type {Job}
+  **/
   onlineJob: onlineJob,
+  /**
+  @private
+  @property offlineJob
+  @type {Job}
+  **/
   offlineJob: offlineJob,
-
+  /**
+  The period of time during which record is considered to be not expired.
+  @property recordTTL
+  @type {Object}
+  **/
   recordTTL: moment.duration(12, 'hours'),
+  /**
+  Used by adapter to get queue.
 
-  lastTimeFetched: Ember.Object.create({}),
-
+  Returns the working queue from an adapter. If there is no queue in adapter then returns queue from passed store.
+  @private
+  @method _workingQueue
+  @param store {DS.Store}
+  @returns {Queue}
+  **/
   _workingQueue(store) {
     if (isPresent(get(this, 'EDOQueue'))) {
       return get(this, 'EDOQueue');
@@ -22,10 +55,30 @@ export default Mixin.create({
     }
   },
 
+  /**
+  Adds job to queue. If onDemandKey param was passed, then job will be stored as 'onDemand'
+  and will be processed on demand.
+
+  @method addToQueue
+  @param job {Job} job to add to queue.
+  @param store {DS.Store}
+  @param onDemandKey {String}
+  **/
   addToQueue(job, store, onDemandKey) {
     this._workingQueue(store).add(job, onDemandKey);
   },
 
+  /**
+  Used by classes which use this class.
+
+  Creates the online job and addes it to queue. If onDemandKey param was passed, then job will be stored as 'onDemand'
+  and will be processed on demand.
+
+  @method createOnlineJob
+  @param method {String} the name of method which will be runned by job.
+  @param params {Array}
+  @param onDemandKey {String}
+  **/
   createOnlineJob(method, params, onDemandKey) {
     let [store, typeClass] = params;
     let job = this.get('onlineJob').create({
@@ -36,7 +89,17 @@ export default Mixin.create({
     });
     this.addToQueue(job, store, onDemandKey);
   },
+  /**
+  Used by classes which use this class.
 
+  Creates the offline job and addes it to queue. If onDemandKey param was passed, then job will be stored as 'onDemand'
+  and will be processed on demand.
+
+  @method createOfflineJob
+  @param method {String} the name of method which will be runned by job.
+  @param params {Array}
+  @param onDemandKey {String}
+  **/
   createOfflineJob(method, params, store) {
     let job = this.get('offlineJob').create({
       adapter: this.get('offlineAdapter'),
@@ -62,4 +125,3 @@ export default Mixin.create({
     $(window).off('offline');
   }),
 });
-
--- addon/queue.js
+++ addon/queue.js
@@ -1,14 +1,69 @@
+/**
+  @module ember-data-offline
+*/
 import Ember from 'ember';
+/**
+## Using Queue
 
+### Example
+
+@class Queue
+@constructor
+@extends Ember.Object
+**/
 export default Ember.Object.extend({
+  /**
+  @property name
+  @type {String}
+  **/
   name: 'normal',
+  /**
+  In situation when the job perfoming fails the queue will try process the job again but with delay
+  which is setted by retryOnFailureDelay param in milliseconds.
+  @property retryOnFailureDelay
+  @type {Number}
+  **/
   retryOnFailureDelay: 5000,
+  /**
+  @property delay
+  @type {Number}
+  **/
   delay: 500,
+  /**
+  Number of workers in a queue.
+  @property workers
+  @type {Number}
+  **/
   workers: 5,
+  /**
+  An array of active jobs in a queue.
+  @property activeJobs
+  @type {Ember.Array}
+  **/
   activeJobs: null,
+  /**
+  An array of pending jobs in a queue.
+  @property pendingJobs
+  @type {Ember.Array}
+  **/
   pendingJobs: null,
+  /**
+  An array of failure jobs in a queue.
+  @property failureJobs
+  @type {Ember.Array}
+  **/
   failureJobs: null,
+  /**
+  An array of retry jobs in a queue.
+  @property retryJobs
+  @type {Ember.Array}
+  **/
   retryJobs: null,
+  /**
+  Saves the jobs that will be processed on demand.
+  @property onDemandJobs
+  @type {Ember.Object}
+  **/
   onDemandJobs: null,
 
   init: function() {
@@ -22,13 +77,22 @@ export default Ember.Object.extend({
     this._super.apply(this, arguments);
   },
 
+  /**
+  @method runJob
+  @param Job {Job}
+  **/
   runJob(job) {
     this.get('activeJobs').pushObject(job);
     Ember.run.later(() => {
       this.process(job);
     }, this.get('delay'));
   },
-
+  /**
+  Checks if the job is exist in a queue.
+  @method isJobExist
+  @param Job {Job}
+  @return {boolean}
+  **/
   isJobExist(job) {
     let pendingJob = this.get('pendingJobs').find((item) => {
       return item.get('adapter') === job.get('adapter') && item.get('method') === job.get('method');
@@ -38,6 +102,7 @@ export default Ember.Object.extend({
     });
     return pendingJob || retryJob;
   },
+
 
   pendingJobObserver: Ember.observer('pendingJobs.[]','activeJobs.[]', function() {
     if (this.get('pendingJobs.length') <= 0) {
@@ -51,6 +116,15 @@ export default Ember.Object.extend({
     }
   }),
 
+  /**
+  Adds job to queue. if onDemandKey param was passed, then job will be stored as 'onDemand'
+  and will be processed on demand.
+
+  @method add
+  @param Job {Job}  job to add
+  @param onDemandKey {String}
+  @return {Job} the passed Job
+  **/
   add: function(job, onDemandKey) {
     if (!Ember.isEmpty(onDemandKey)) {
       return this.set(`onDemandJobs.${onDemandKey}`, job);
@@ -60,10 +134,24 @@ export default Ember.Object.extend({
     }
   },
 
+  /**
+  Removes the job from queue.
+
+  @method remove
+  @param Job {Job}  job to remove
+  **/
   remove: function(job) {
     this.get('pendingJobs').removeObject(job);
   },
 
+  /**
+  Performs the job. After successful processing removes the job from list of active and retry jobs.
+  If job has failed, it should be automatically retried with a delay if retry attempts will be available
+  (every job has a retry count which decrements by 1 after  every retry attempt).
+
+  @method process
+  @param Job {Job} job to process
+  **/
   process: function(job) {
     let queue = this;
     job.perform().then(() => {
